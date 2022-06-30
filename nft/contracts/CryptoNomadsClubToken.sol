6047d8539fd0c55f33ae8df7c776ddd0b983d8f8
Ivan Porollo <iporollo@gmail.com>
2022-06-30 16:54:03 -0700

    finilaze intial contract implementation

--- nft/contracts/CryptoNomadsClubToken.sol
+++ nft/contracts/CryptoNomadsClubToken.sol
@@ -16,15 +16,16 @@ contract CryptoNomadsClub is
 
     // PUBLIC TOKENS AMOUNT = 2900;
     // SOUL BOUND TOKENS AMOUNT = 100;
-    // MAX TOKENS THAT CAN BE MINTED BY AN ADDRESS = 5;
 
     uint256 private publicAmount;
     uint256 private soulBoundCounter;
 
+    uint256 public availableToMint;
+    uint256 public price;
+
     mapping(string => bool) public CITIES;
 
     constructor() ERC721("Crypto Nomads Club", "CNC") {
-        publicAmount = 0;
         soulBoundCounter = 2900;
 
         // create a map of city names so we know which names can be minted
@@ -45,11 +46,17 @@ contract CryptoNomadsClub is
         CITIES["SINGAPORE"] = true;
     }
 
-    // function setBaseURI(string memory baseURI) external onlyOwner {
-    //     _setBaseURI(baseURI);
-    // }
+    function setBatch(
+        // params can be a struct
+        uint256 batchSize,
+        uint256 batchPrice
+    ) external onlyOwner {
+        availableToMint = batchSize;
+        price = batchPrice;
+    }
 
-    function mintCNC(string[] memory cities) external payable {
+    function mint(string[] memory cities) external payable {
+        require(availableToMint >= cities.length, "Batch sold out");
         require(totalSupply() < MAX_TOKENS, "Sold out");
         require(publicAmount < 2900, "All public tokens sold out");
         require(
@@ -60,30 +67,34 @@ contract CryptoNomadsClub is
             publicAmount + cities.length <= 2900,
             "Not enough public tokens available"
         );
-        require(0.3 ether * cities.length <= msg.value, "Insufficient ETH");
+        require(price * cities.length <= msg.value, "Insufficient ETH");
 
         for (uint256 i = 0; i < cities.length; i++) {
             require(CITIES[cities[i]], "Invalid city");
-            publicAmount++;
-            _safeMint(msg.sender, publicAmount);
+            _safeMint(msg.sender, publicAmount + i);
             string memory finalTokenURI = string(
-                abi.encodePacked(_baseURI(), cities[i], ".json")
+                abi.encodePacked(cities[i], ".json")
             );
             _setTokenURI(publicAmount, finalTokenURI);
         }
+
+        publicAmount += cities.length;
+        availableToMint -= cities.length;
     }
 
     function gift(address receiver, string memory city) external onlyOwner {
         require(totalSupply() < MAX_TOKENS, "Sold out");
-        require(soulBoundCounter <= 100, "Run out of soul bound tokens");
+        require(soulBoundCounter <= 3000, "Run out of soul bound tokens");
         require(CITIES[city], "Invalid city");
 
         _mint(receiver, soulBoundCounter);
-        string memory finalTokenURI = string(
-            abi.encodePacked(_baseURI(), city, ".json")
-        );
+        string memory finalTokenURI = string(abi.encodePacked(city, ".json"));
         _setTokenURI(soulBoundCounter, finalTokenURI);
         soulBoundCounter++;
+    }
+
+    function withdrawAll() external onlyOwner {
+        payable(msg.sender).transfer(address(this).balance);
     }
 
     // Overriding _baseURI
@@ -95,7 +106,7 @@ contract CryptoNomadsClub is
         override(ERC721)
         returns (string memory)
     {
-        return "ipfs://<folder-hash-code";
+        return "ipfs://QmRgvuYv5Y4qjQfXz7KshzQpSne1kM5dP4jAWuFyyTzZGH";
     }
 
     // Overriding transfer hook to check for soulbound
@@ -107,12 +118,8 @@ contract CryptoNomadsClub is
     ) internal override(ERC721, ERC721Enumerable) {
         // if sender is a 0 address, this is a mint transaction, not a transfer
         require(
-            tokenId < 2900 && from == address(0),
+            tokenId < 2900 || from == address(0),
             "ERROR: TOKEN IS SOUL BOUND"
-        );
-        require(
-            balanceOf(to) < 5,
-            "Maximum amount of NFTs minted for this wallet"
         );
 
         super._beforeTokenTransfer(from, to, tokenId);
