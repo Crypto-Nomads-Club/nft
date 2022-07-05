a2b46b9c5950a5fe27c6f2b0e755a566a3ea8f30
Ivan Porollo <iporollo@gmail.com>
2022-07-05 15:46:45 -0700

    update hardhat config

--- nft/hardhat.config.js
+++ nft/hardhat.config.js
@@ -1,17 +1,20 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-require('dotenv').config();
-require('@nomiclabs/hardhat-ethers');
+require('dotenv').config({ path: __dirname + '/.env' });
+require('@nomiclabs/hardhat-waffle');
 const { API_URL, PRIVATE_KEY } = process.env;
 module.exports = {
-  solidity: '0.8.1',
-  defaultNetwork: 'ropsten',
+  solidity: '0.8.7',
+  defaultNetwork: 'hardhat',
   networks: {
-    hardhat: {},
-    ropsten: {
-      url: API_URL,
-      accounts: [`0x${PRIVATE_KEY}`],
+    hardhat: {
+      accounts: {
+        accountsBalance: '1000000000000000000000000000000',
+      },
     },
+  },
+  mocha: {
+    timeout: 100000000,
   },
 };
